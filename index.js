06f8bd1e837b56a1d6c924810104a9420a790fb3
BananaCakeCN <103292046+BananaCakeCN@users.noreply.github.com>
2024-05-13 16:04:35 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -33,10 +33,92 @@ function drawPattern(n, singleW, label){
     }
     return '<svg style="border: 3px solid #000;" width="' + ((1+n)*n/2)*singleW + '" height="' + ((1+n)*n/2)*singleW + '">' + res + '</svg>';
 }
+function drawPatternCVS(n, singleW, label, canvas){
+    canvas.width = ((1+n)*n/2)*singleW;
+    canvas.height = ((1+n)*n/2)*singleW;
+    draw = canvas.getContext('2d');
+    draw.fillStyle = '#000';
+    draw.strokeStyle = '#000';
+    for(var i = 1; i <= n; i++){
+        draw.lineWidth = 1;
+        for(var j = (i*(i-1)/2)*singleW - i * singleW; j >= 0; j -= singleW * i){
+            draw.strokeRect(j, (i*(i-1)/2)*singleW, i*singleW, i*singleW);
+            draw.strokeRect((i*(i-1)/2)*singleW, j, i*singleW, i*singleW);
+        }
+        if(i % 2 == 0){
+            draw.strokeRect(0, (i*(i-1)/2)*singleW, i*singleW/2, i*singleW);
+            draw.strokeRect((i*(i-1)/2)*singleW, 0, i*singleW, i*singleW/2);
+        }
+        draw.lineWidth = 3;
+        draw.strokeRect((i*(i-1)/2)*singleW, (i*(i-1)/2)*singleW, i*singleW, i*singleW);
+        draw.lineWidth = 2;
+        draw.beginPath();
+        draw.moveTo(0, (i*(i-1)/2)*singleW);
+        draw.lineTo((i*(i-1)/2)*singleW, (i*(i-1)/2)*singleW);
+        draw.moveTo((i*(i-1)/2)*singleW, 0);
+        draw.lineTo((i*(i-1)/2)*singleW, (i*(i-1)/2)*singleW);
+        draw.stroke();
+        if(label){
+            draw.fillText(i, ((i*(i-1)/2)*singleW+3), ((i*(i-1)/2)*singleW+15))
+        }
+        for(var j = singleW; j < i * singleW; j += singleW){
+            draw.beginPath();
+            draw.moveTo(((i*(i-1)/2)*singleW + j), (i*(i-1)/2)*singleW);
+            draw.lineTo(((i*(i-1)/2)*singleW + j), ((i*(i-1)/2)*singleW + i * singleW));
+            draw.moveTo((i*(i-1)/2)*singleW, ((i*(i-1)/2)*singleW + j));
+            draw.lineTo(((i*(i-1)/2)*singleW + i * singleW), ((i*(i-1)/2)*singleW + j));
+            draw.stroke();
+        }
+    }
+}
 document.getElementById('example').innerHTML = drawPattern(5, 30, false);
 document.getElementById('explainGraph').innerHTML += drawPattern(6, 20, true);
 document.getElementById('startBTN').onclick = async function(){
     document.getElementById('start').style.cssText = 'animation: hide 0.3s ease forwards;';
     await sleep(300);
     document.getElementById('start').remove();
+    document.getElementsByClassName('graphMain1')[0].style.fill = '#0000CD';
+    document.getElementsByClassName('graphMain2')[0].style.fill = '#00FFFF';
+    for(ele of document.getElementsByClassName('graphSub2')){
+        ele.style.fill = '#00CED1';
+    }
+    document.getElementsByClassName('graphMain3')[0].style.fill = '#3CB371';
+    for(ele of document.getElementsByClassName('graphSub3')){
+        ele.style.fill = '#2E8B57';
+    }
+    document.getElementsByClassName('graphMain4')[0].style.fill = '#ffff00';
+    for(ele of document.getElementsByClassName('graphSub4')){
+        ele.style.fill = '#BDB76B';
+    }
+    document.getElementsByClassName('graphMain5')[0].style.fill = '#FF0000';
+    for(ele of document.getElementsByClassName('graphSub5')){
+        ele.style.fill = '#A52A2A';
+    }
+    document.getElementsByClassName('graphMain6')[0].style.fill = '#666';
+    for(ele of document.getElementsByClassName('graphSub6')){
+        ele.style.fill = '#333';
+    }
+    document.getElementById('example1').innerHTML = drawPattern(5, 30, true);
+    document.getElementById('number1').oninput = function(){
+        document.getElementById('example1').innerHTML = drawPattern(parseInt(document.getElementById('number1').value), 30, false);
+    }
+    drawPatternCVS(5, 30, true, document.getElementById('example2'))
+    document.getElementById('number2').oninput = function(){
+        drawPatternCVS(parseInt(document.getElementById('number2').value), 30, true, document.getElementById('example2'))
+    }
+}
+document.onscroll = function(){
+    if(document.scrollingElement.scrollTop > document.getElementById('scroll').offsetTop){
+        document.getElementById('explainGraph').style.display = 'none';
+    }else{
+        document.getElementById('explainGraph').style.display = '';
+    }
+}
+async function showHow(){
+    while(document.getElementsByClassName('label')[5].getAttribute('y') >= 15){
+        for(var i = 0; i < document.getElementsByClassName('label').length; i++){
+            document.getElementsByClassName('label')[i].setAttribute('y', document.getElementsByClassName('label')[i].getAttribute('y') * 0.95 + (document.getElementsByClassName('label').length - i)/10);
+        }
+        await sleep(20)
+    }
 }
